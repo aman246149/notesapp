d8509d824d118ce8325210645efc5e36556129df
aman <amanthapliyal14>
2022-06-05 09:20:48 +0530

    search screenimplemented

--- lib/Screens/AddNoteScreen.dart
+++ lib/Screens/AddNoteScreen.dart
@@ -38,164 +38,168 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      floatingActionButton: CustomFloatingActionButton(
-        note: _noteController.text,
-        context: context,
-        size: size,
-        customDialog:
-            customDialog(context, _noteController.text, selectedColorCode),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        // color: Colors.amber,
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 18.0, left: 18, right: 18),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                        onTap: () => Navigator.of(context).pop(),
-                        child: const Icon(Icons.arrow_back)),
-                    Row(
-                      children: const [
-                        Icon(Icons.pin),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Icon(Icons.notifications),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Icon(Icons.menu),
-                        SizedBox(
-                          width: 10,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                "Cookies",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 18.0, left: 0, right: 0),
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 1.200000000,
-                  // color: Colors.green,
-                  child: Stack(
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: CustomFloatingActionButton(
+          note: _noteController.text,
+          context: context,
+          size: size,
+          customDialog:
+              customDialog(context, _noteController.text, selectedColorCode),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          // color: Colors.amber,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 18.0, left: 18, right: 18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height / 2,
-                        width: MediaQuery.of(context).size.width,
-                        child: TextField(
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                          controller: _noteController,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Add Note',
-                              hintStyle: TextStyle(fontSize: 20)),
-                          keyboardType: TextInputType.multiline,
-                          minLines: 1,
-                          maxLines: 5,
-
-                          // cursorHeight: 50,
-                        ),
+                      GestureDetector(
+                          onTap: () => Navigator.of(context).pop(),
+                          child: const Icon(Icons.arrow_back)),
+                      Row(
+                        children: const [
+                          Icon(Icons.pin),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Icon(Icons.notifications),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Icon(Icons.menu),
+                          SizedBox(
+                            width: 10,
+                          ),
+                        ],
                       ),
-                      Positioned.fill(
-                        child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Stack(
-                              children: [
-                                Container(
-                                  height: 120,
-                                  width: MediaQuery.of(context).size.width,
-                                  child: Card(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    elevation: 5,
-                                    child: ListView.builder(
-                                      scrollDirection: Axis.horizontal,
-                                      itemCount: colors.length,
-                                      itemBuilder: (context, index) {
-                                        return Padding(
-                                          padding: const EdgeInsets.only(
-                                            top: 30.0,
-                                            bottom: 30,
-                                            left: 20,
-                                          ),
-                                          child: GestureDetector(
-                                            onTap: (() {
-                                              print(index);
-                                            }),
-                                            child: Stack(
-                                              children: [
-                                                CustomContainerColor(
-                                                  colorValue: colors[index]
-                                                      ["color"],
-                                                ),
-                                                Checkbox(
-                                                  value: colors[index]
-                                                      ["isChecked"],
-                                                  onChanged: (value) => {
-                                                    for (var i in colors)
-                                                      {
-                                                        if (i.containsValue(
-                                                            true))
-                                                          {
-                                                            i["isChecked"] =
-                                                                false
-                                                          }
-                                                      },
-                                                    setState(() {
-                                                      colors[index]
-                                                              ["isChecked"] =
-                                                          value!;
-                                                    }),
-                                                    selectedColorCode =
-                                                        colors[index]["color"],
-                                                    print(selectedColorCode
-                                                        .toString())
-                                                  },
-                                                  fillColor:
-                                                      MaterialStateProperty.all(
-                                                          Colors.transparent),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )),
-                      )
                     ],
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 25,
+                ),
+                const Text(
+                  "Cookies",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18.0, left: 0, right: 0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 1.200000000,
+                    // color: Colors.green,
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height / 2,
+                          width: MediaQuery.of(context).size.width,
+                          child: TextField(
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                            controller: _noteController,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Add Note',
+                                hintStyle: TextStyle(fontSize: 20)),
+                            keyboardType: TextInputType.multiline,
+                            minLines: 1,
+                            maxLines: 5,
+
+                            // cursorHeight: 50,
+                          ),
+                        ),
+                        Positioned.fill(
+                          child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    height: 120,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: Card(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      elevation: 5,
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: colors.length,
+                                        itemBuilder: (context, index) {
+                                          return Padding(
+                                            padding: const EdgeInsets.only(
+                                              top: 30.0,
+                                              bottom: 30,
+                                              left: 20,
+                                            ),
+                                            child: GestureDetector(
+                                              onTap: (() {
+                                                print(index);
+                                              }),
+                                              child: Stack(
+                                                children: [
+                                                  CustomContainerColor(
+                                                    colorValue: colors[index]
+                                                        ["color"],
+                                                  ),
+                                                  Checkbox(
+                                                    value: colors[index]
+                                                        ["isChecked"],
+                                                    onChanged: (value) => {
+                                                      for (var i in colors)
+                                                        {
+                                                          if (i.containsValue(
+                                                              true))
+                                                            {
+                                                              i["isChecked"] =
+                                                                  false
+                                                            }
+                                                        },
+                                                      setState(() {
+                                                        colors[index]
+                                                                ["isChecked"] =
+                                                            value!;
+                                                      }),
+                                                      selectedColorCode =
+                                                          colors[index]
+                                                              ["color"],
+                                                      print(selectedColorCode
+                                                          .toString())
+                                                    },
+                                                    fillColor:
+                                                        MaterialStateProperty
+                                                            .all(Colors
+                                                                .transparent),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:notetakingapp/Model/Note.dart';
+import 'package:notetakingapp/Widgets/HomePageWidgets/CustomCards.dart';
+import 'package:notetakingapp/Widgets/HomePageWidgets/CustomHomeScreenDialog.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -8,10 +14,95 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _controller = TextEditingController();
+  bool isShowUser = false;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Search")),
-    );
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          title: TextFormField(
+            controller: _controller,
+            decoration: const InputDecoration(
+                labelText: 'Search for a Note...',
+                labelStyle: TextStyle(color: Colors.black)),
+            onChanged: (String val) {
+              setState(() {
+                isShowUser = true;
+              });
+            },
+            onFieldSubmitted: (String _) {
+              setState(() {
+                isShowUser = true;
+              });
+            },
+          ),
+        ),
+        body: isShowUser
+            ? StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("Notes")
+                    .doc(FirebaseAuth.instance.currentUser!.uid)
+                    .collection("userIndNotes")
+                    .where('note', isGreaterThanOrEqualTo: _controller.text)
+                    .snapshots(),
+                builder: (context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snapshots) {
+                  if (snapshots.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return GridView.custom(
+                    gridDelegate: SliverWovenGridDelegate.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                      pattern: [
+                        const WovenGridTile(1),
+                        const WovenGridTile(
+                          5 / 6,
+                          crossAxisRatio: 0.9,
+                          alignment: AlignmentDirectional.centerEnd,
+                        ),
+                      ],
+                    ),
+                    childrenDelegate: SliverChildBuilderDelegate(
+                      childCount: snapshots.data!.docs.length,
+                      (context, index) {
+                        Note note = Note.fromSnap(snapshots.data!.docs[index]);
+                        return Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (dialogContext) => openDialogBox(
+                                        dialogContext,
+                                        note,
+                                        snapshots.data!.docs[index].id,
+                                        true));
+                              },
+                              child: CustomCard(note: note)),
+                        );
+                      },
+                    ),
+                  );
+                },
+              )
+            : Center(
+                child: Text("Search For Your Note"),
+              ));
   }
 }
